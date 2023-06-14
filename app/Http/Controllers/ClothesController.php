12a529f9f0d7dab06685854c779dac4d29773a16
Ugwu Obed <vjazzyvictor@gmail.com>
2023-06-14 20:42:51 +0100

    fest

--- app/Http/Controllers/ClothesController.php
+++ app/Http/Controllers/ClothesController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use App\Models\Clothes;
 use Illuminate\Support\Facades\Storage;
 
-
 class ClothesController extends Controller
 {
     /**
@@ -14,7 +13,8 @@ class ClothesController extends Controller
      */
     public function index()
     {
-        $clothes = Clothes::all();
+        $user = auth()->user();
+        $clothes = $user->clothes;
         return view('clothes.index', compact('clothes'));
     }
 
@@ -29,58 +29,63 @@ class ClothesController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        // Validate the request data
-        $validatedData = $request->validate([
-            'name' => 'required|string',
-            'type' => 'required|string',
-            'color' => 'required|string',
-            'size' => 'required|string',
-            'category' => 'nullable|string',
-            'gender' => 'nullable|string',
-            'brand' => 'required|string',
-            'price' => 'required|numeric',
-            'description' => 'nullable|string',
-            'image_url1' => 'nullable|image|max:2048',
-            'image_url2' => 'nullable|image|max:2048',
-            'image_url3' => 'nullable|image|max:2048',
-        ]);
+public function store(Request $request)
+{
+    // Validate the request data
+    $validatedData = $request->validate([
+        'name' => 'required|string',
+        'type' => 'required|string',
+        'color' => 'required|string',
+        'size' => 'required|string',
+        'category' => 'nullable|string',
+        'gender' => 'nullable|string',
+        'brand' => 'required|string',
+        'price' => 'required|numeric',
+        'description' => 'nullable|string',
+        'image_url1' => 'nullable|image|max:2048',
+        'image_url2' => 'nullable|image|max:2048',
+        'image_url3' => 'nullable|image|max:2048',
+    ]);
 
-        // Handle the image uploads if provided
-        $imagePaths = [];
-        if ($request->hasFile('image_url1')) {
-            $imagePaths[] = $request->file('image_url1')->store('clothes');
-        }
-        if ($request->hasFile('image_url2')) {
-            $imagePaths[] = $request->file('image_url2')->store('clothes');
-        }
-        if ($request->hasFile('image_url3')) {
-            $imagePaths[] = $request->file('image_url3')->store('clothes');
-        }
-
-        // Create a new Clothes instance
-        $clothes = new Clothes([
-            'name' => $validatedData['name'],
-            'type' => $validatedData['type'],
-            'color' => $validatedData['color'],
-            'size' => $validatedData['size'],
-            'category' => $validatedData['category'],
-            'gender' => $validatedData['gender'],
-            'brand' => $validatedData['brand'],
-            'price' => $validatedData['price'],
-            'description' => $validatedData['description'],
-            'image_url1' => $imagePaths[0] ?? null,
-            'image_url2' => $imagePaths[1] ?? null,
-            'image_url3' => $imagePaths[2] ?? null,
-        ]);
-
-        // Save the Clothes instance
-        auth()->user()->clothes()->save($clothes);
-
-        // Redirect to the index page or show a success message
-        return redirect()->route('clothes.index')->with('success', 'Clothes added successfully.');
+    // Handle the image uploads if provided
+    $imagePaths = [];
+    if ($request->hasFile('image_url1')) {
+        $imagePaths[] = $request->file('image_url1')->store('clothes');
     }
+    if ($request->hasFile('image_url2')) {
+        $imagePaths[] = $request->file('image_url2')->store('clothes');
+    }
+    if ($request->hasFile('image_url3')) {
+        $imagePaths[] = $request->file('image_url3')->store('clothes');
+    }
+     
+    // Get the authenticated user
+    $user = auth()->user();
+
+    // Create a new Clothes instance
+    $clothes = new Clothes([
+        'name' => $validatedData['name'],
+        'type' => $validatedData['type'],
+        'color' => $validatedData['color'],
+        'size' => $validatedData['size'],
+        'category' => $validatedData['category'],
+        'gender' => $validatedData['gender'],
+        'brand' => $validatedData['brand'],
+        'price' => $validatedData['price'],
+        'description' => $validatedData['description'],
+        'image_url1' => $imagePaths[0] ?? null,
+        'image_url2' => $imagePaths[1] ?? null,
+        'image_url3' => $imagePaths[2] ?? null,
+        'user_id' => $user->id,
+    ]);
+
+    // Save the Clothes instance
+    auth()->user()->clothes()->save($clothes);
+
+    // Redirect to the index page or show a success message
+    return redirect()->route('clothes.index')->with('success', 'Clothes added successfully.');
+}
+
 
     /**
      * Display the specified resource.
@@ -96,7 +101,7 @@ class ClothesController extends Controller
      */
     public function edit($id)
     {
-        $clothes = Clothes::findOrFail($id);
+        $clothes = auth()->user()->clothes()->findOrFail($id);
         return view('clothes.edit', compact('clothes'));
     }
 
@@ -106,7 +111,7 @@ class ClothesController extends Controller
     public function update(Request $request, $id)
     {
         // Find the Clothes instance
-        $clothes = Clothes::findOrFail($id);
+        $clothes = auth()->user()->clothes()->findOrFail($id);
 
         // Validate the request data
         $validatedData = $request->validate([
@@ -168,7 +173,7 @@ class ClothesController extends Controller
     public function destroy($id)
     {
         // Find the Clothes instance
-        $clothes = Clothes::findOrFail($id);
+        $clothes = auth()->user()->clothes()->findOrFail($id);
 
         // Delete the image files associated with the Clothes instance if they exist
         $imageUrls = [$clothes->image_url1, $clothes->image_url2, $clothes->image_url3];
